cc47121dc55ab27255713d3123045222cf7d7c66
jose-salguero <jsalgueroc@miumg.edu.gt>
2021-11-05 21:24:04 -0600

    Prueba

--- conexion/conection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function conexion()
-	{
-		return $conexion=mysqli_connect("localhost","root","2009000582","red_social");
-	}
-	?>
--- javaScript/mi_perfil.js
+++
@@ -1,3 +0,0 @@
-	$("#file").change(function(){
-    $("#btn-Agregar").prop("disabled", this.files.length == 0);
-});
