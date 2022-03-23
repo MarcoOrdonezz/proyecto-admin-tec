eba229b48403fe7fd056dee57ae90244c2871029
Marco Ordonez <63825047+MarcoOrdonezz@users.noreply.github.com>
2022-03-22 18:38:41 -0600

    Actualizacion

--- requests.php
+++ requests.php
@@ -1,6 +1,6 @@
 <?php
 // +------------------------------------------------------------------------+
-// | @author Deen Doughouz (DoughouzForest)
+// |
 // | @author_url 1: http://www.wowonder.com
 // | @author_url 2: http://codecanyon.net/user/doughouzforest
 // | @author_email: wowondersocial@gmail.com   
